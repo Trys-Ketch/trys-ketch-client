f534cec8eb53233362af78ca1dddbf848ba3d6a1
Jdentity <identityjang@gmail.com>
2023-01-03 18:03:43 +0900

    👔 Add axios instance axios 인스턴스 추가

---
+++ src/api/core/axios.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { getCookie } from '../../utils/cookie';
+
+// 인스턴스 생성
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+});
+
+// 요청 타임아웃
+instance.defaults.timeout = 2500;
+
+// 인스턴스 request header Authorization 설정
+instance.interceptors.request.use((config) => {
+  if (config.headers === undefined) return;
+  const token = getCookie();
+  config.headers.Authorization = `${token}`;
+  return config;
+});
+
+// TODO - 로그인 만료 처리 interceptors
+// Unauthorized Error 처리
+// axios.interceptors.response.use((error) => {
+//   if (error.response.status === 401) {
+//     useToast('로그인이 만료되었습니다.', 'error');
+//     window.location.href('/login');
+//   }
+// });
+
+export default instance;
