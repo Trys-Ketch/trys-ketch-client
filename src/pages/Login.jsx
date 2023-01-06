23f1177cf79cdf3af8c6bb7d1c6fa6f07e32f502
Jdentity <identityjang@gmail.com>
2023-01-06 10:02:50 +0900

    💄 Add guest login btn 로그인 페이지에 게스트 로그인 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import KakaoLoginBtn from '../components/login/KakaoLoginBtn';
+import Button from '../components/common/Button';
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <div>
       <KakaoLoginBtn />
+      <Button
+        onClick={() => {
+          navigate('/guest');
+        }}
+      >
+        체험하기
+      </Button>
     </div>
   );
 }
