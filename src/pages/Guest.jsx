982cb05e7146a2ae3600740e9ec9f00e5c0d00d7
Jdentity <identityjang@gmail.com>
2023-01-10 17:13:51 +0900

    ✨ Add get random nickname in guest page 게스트 페이지에서 랜덤닉네임 가져오는 기능 추가

--- src/pages/Guest.jsx
+++ src/pages/Guest.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import Avatar from '../components/common/Avatar';
 import Input from '../components/common/Input';
 import Button from '../components/common/Button';
+import authAPI from '../api/auth';
+import userAPI from '../api/user';
 
 function Guest() {
   const [nickname, setNickname] = useState('');
@@ -13,15 +15,36 @@ function Guest() {
     setNickname(e.target.value);
   };
 
-  const handleSubmit = () => {
-    // guest 입장 api
-    navigate('/');
+  const handleSubmit = async () => {
+    if (nickname === '') {
+      alert('닉네임을 입력해주세요');
+    }
+
+    const trimedName = nickname.trim();
+    await authAPI.guestLogin(trimedName).then((res) => {
+      // TODO - 게스트 로그인 response.data.header.guest 쿠키 처리 필요
+      if (res.data.statusCode === 200) {
+        navigate('/');
+        alert('게스트로그인 성공');
+      }
+    });
   };
+
+  const getRandomNickname = async () => {
+    await userAPI.getRandomNickname().then((res) => {
+      setNickname(res.data.message);
+    });
+  };
+
+  useEffect(() => {
+    getRandomNickname();
+  }, []);
 
   return (
     <Layout>
       <Avatar width="68px" height="68px" />
       <Input value={nickname} onChange={handleInput} />
+      <Button onClick={getRandomNickname}>새로고침</Button>
       <Button onClick={handleSubmit}>완료</Button>
     </Layout>
   );
