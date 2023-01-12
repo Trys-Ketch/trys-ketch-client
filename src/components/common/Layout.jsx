178148008afc00c1e9fb98f1f1798c569f53522a
Jdentity <identityjang@gmail.com>
2023-01-13 02:20:08 +0900

    💄 Add layout component Layout, Panel 추가

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import background from '../../assets/images/background.png';
+
+const Layout = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom;
+`;
+
+export default Layout;
