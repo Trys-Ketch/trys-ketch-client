6c4c8da5351e2c837cffb1dc68532bfa67ec2697
Jdentity <identityjang@gmail.com>
2023-01-13 02:45:54 +0900

    💄 Add Login page style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,63 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import KakaoLoginBtn from '../components/login/KakaoLoginBtn';
+import styled from 'styled-components';
 import Button from '../components/common/Button';
+import KakaoLoginBtn from '../components/login/KakaoLoginBtn';
+import NaverLoginBtn from '../components/login/NaverLoginBtn';
+import GoogleLoginBtn from '../components/login/GoogleLoginBtn';
+import Layout from '../components/common/Layout';
+import Panel from '../components/common/Panel';
+import logo from '../assets/images/logo.png';
 
 function Login() {
   const navigate = useNavigate();
   return (
-    <div>
-      <KakaoLoginBtn />
-      <Button
-        onClick={() => {
-          navigate('/guest');
-        }}
-      >
-        체험하기
-      </Button>
-    </div>
+    <Layout>
+      <Panel>
+        <Logo src={logo} alt="logo" />
+        <Typography>간편로그인</Typography>
+        <ButtonBox>
+          <KakaoLoginBtn />
+          <GoogleLoginBtn />
+          <NaverLoginBtn />
+        </ButtonBox>
+        <Button
+          onClick={() => {
+            navigate('/guest');
+          }}
+          width="350px"
+        >
+          게스트 로그인
+        </Button>
+      </Panel>
+    </Layout>
   );
 }
+
+const Logo = styled.img`
+  max-width: 600px;
+  min-width: 450px;
+  width: 50%;
+  margin-bottom: 30px;
+`;
+
+const Typography = styled.span`
+  color: '#4e473f';
+  font-weight: 600;
+`;
+
+const ButtonBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 30px;
+
+  & div:not(:first-child) {
+    margin-left: 30px;
+  }
+`;
 
 export default Login;
