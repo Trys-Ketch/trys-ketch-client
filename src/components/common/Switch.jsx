a7ac5881f4cdf539d19531c4597227450f6758f4
Jdentity <identityjang@gmail.com>
2023-01-16 12:10:01 +0900

    🚧 Working range, switch Range 컴포넌트, Switch 컴포넌트 제작 중

---
+++ src/components/common/Switch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Switch() {
+  return <ToggleSwitch type="checkbox" />;
+}
+
+const ToggleSwitch = styled.input``;
+
+export default Switch;
