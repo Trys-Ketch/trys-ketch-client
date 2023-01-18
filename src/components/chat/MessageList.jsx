d8d82cdfe26d98145c52da20bdbb04c8d177889f
Jdentity <identityjang@gmail.com>
2023-01-18 21:49:11 +0900

    ✨ Feat scroll to bottom when chat 채팅할 때 스크롤 다운 기능 추가

--- src/components/chat/MessageList.jsx
+++ src/components/chat/MessageList.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Message from './Message';
 
 function MessageList({ messages }) {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <StMessageList>
       {messages.map((message, idx) => (
         <Message key={idx} message={message} />
       ))}
+      <div ref={messagesEndRef} />
     </StMessageList>
   );
 }
