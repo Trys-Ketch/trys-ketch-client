a826577ab62ef60e95111aa516313f24897189e6
Jdentity <identityjang@gmail.com>
2023-01-18 21:40:37 +0900

    💄 Add chat styles 채팅 스타일 추가

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,48 +1,96 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import Avatar from '../common/Avatar';
 
 function Message({ message }) {
-  return (
-    <StMessage className={message.type}>
-      {message.type === 'my' ? (
-        <>
+  const { userId } = useSelector((state) => state.user);
+
+  switch (message.type) {
+    case 'NOTICE':
+      return (
+        <Notice>
+          <Body>
+            <Content>{message.content}</Content>
+          </Body>
+        </Notice>
+      );
+    case 'CHAT':
+      if (message.userId === userId) {
+        return (
+          <MyChat>
+            <Body>
+              <Content>{message.content}</Content>
+            </Body>
+            <Avatar src={message.profileImage} width="38px" height="38px" />
+          </MyChat>
+        );
+      }
+      return (
+        <OtherChat>
+          <Avatar src={message.profileImage} width="38px" height="38px" />
           <Body>
             <Nickname>{message.nickname}</Nickname>
             <Content>{message.content}</Content>
           </Body>
-          <Avatar width="32px" height="32px" />
-        </>
-      ) : (
-        <>
-          <Avatar width="32px" height="32px" />
-          <Body>
-            <Nickname>{message.nickname}</Nickname>
-            <Content>{message.content}</Content>
-          </Body>
-        </>
-      )}
-    </StMessage>
-  );
+        </OtherChat>
+      );
+    default:
+      break;
+  }
 }
 
 const StMessage = styled.div`
   display: flex;
   flex-direction: row;
-  &.my {
-    justify-content: flex-end;
-  }
+  align-items: flex-start;
+  margin: 10px 0;
 `;
 
 const Body = styled.div`
   display: flex;
   flex-direction: column;
+  margin: 0 5px;
 `;
 
 const Nickname = styled.span`
+  color: ${({ theme }) => theme.colors.DARK_LAVA};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
 `;
 
-const Content = styled.span``;
+const Content = styled.span`
+  border-radius: 10px;
+  padding: 12px;
+  color: ${({ theme }) => theme.colors.DARK_LAVA};
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+`;
+
+const Notice = styled(StMessage)`
+  justify-content: center;
+
+  ${Content} {
+    color: ${({ theme }) => theme.colors.WHITE};
+    background-color: #48afe9;
+  }
+`;
+
+const MyChat = styled(StMessage)`
+  justify-content: flex-end;
+
+  ${Content} {
+    background-color: ${({ theme }) => theme.colors.WHITE};
+  }
+`;
+
+const OtherChat = styled(StMessage)`
+  justify-content: flex-start;
+
+  ${Content} {
+    width: fit-content;
+    margin-top: 5px;
+    background-color: ${({ theme }) => theme.colors.FLORAL_WHITE};
+  }
+`;
 
 export default Message;
