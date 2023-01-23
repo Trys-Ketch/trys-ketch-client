65f25e8c59917acffb23d61143d6ad53467d541e
GosuEE <leemin727@naver.com>
2023-01-23 23:03:40 +0900

    :adhesive_bandage: Remove Drawing in app.js
    App.js에 Drawing 컴포넌트를 return하던 것 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Drawing from './components/game/Drawing';
 import Router from './shared/Router';
 
 function App() {
-  // return <Router />;
-  return <Drawing />;
+  return <Router />;
 }
 
 export default App;
