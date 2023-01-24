d6a89c8560073fb20078f31276502a9c90ce40ad
Jdentity <identityjang@gmail.com>
2023-01-24 16:12:30 +0900

    ✨  Add sns Logins sns 로그인 모두 추가 params로 각각의 로그인 구분

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect } from 'react';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { setCookie } from '../utils/cookie';
+import { setLogin } from '../app/slices/loginSlice';
+import authAPI from '../api/auth';
 import Button from '../components/common/Button';
 import KakaoLoginBtn from '../components/login/KakaoLoginBtn';
 import NaverLoginBtn from '../components/login/NaverLoginBtn';
@@ -10,6 +14,79 @@ import logo from '../assets/images/ribbon-logo.svg';
 
 function Login() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { sns } = useParams();
+  const [searchParams] = useSearchParams();
+
+  const code = searchParams.get('code');
+  const state = searchParams.get('state');
+
+  const kakaoLogin = useCallback(async () => {
+    await authAPI
+      .kakaoLogin(code)
+      .then((res) => {
+        if (res.data.statusCode === 200) {
+          dispatch(setLogin('kakao'));
+          setCookie(res.headers.authorization);
+          alert(res.data.message);
+          navigate('/');
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch((error) => alert(error.message));
+  }, [code, dispatch, navigate]);
+
+  const googleLogin = useCallback(async () => {
+    await authAPI
+      .googleLogin(code)
+      .then((res) => {
+        if (res.data.statusCode === 200) {
+          dispatch(setLogin('google'));
+          setCookie(res.headers.authorization);
+          alert(res.data.message);
+          navigate('/');
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch((error) => alert(error.message));
+  }, [code, dispatch, navigate]);
+
+  const naverLogin = useCallback(async () => {
+    await authAPI
+      .naverLogin(code, state)
+      .then((res) => {
+        if (res.data.statusCode === 200) {
+          dispatch(setLogin('naver'));
+          setCookie(res.headers.authorization);
+          alert(res.data.message);
+          navigate('/');
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch((error) => alert(error.message));
+  }, [code, dispatch, navigate, state]);
+
+  useEffect(() => {
+    if (code) {
+      switch (sns) {
+        case 'kakao':
+          kakaoLogin();
+          break;
+        case 'google':
+          googleLogin();
+          break;
+        case 'naver':
+          naverLogin();
+          break;
+        default:
+          break;
+      }
+    }
+  }, [googleLogin, kakaoLogin, naverLogin, sns, code]);
+
   return (
     <Panel>
       <Logo src={logo} alt="logo" />
