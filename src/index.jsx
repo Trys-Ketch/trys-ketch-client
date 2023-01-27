16181e14f1177c1d49b4b3240dee72f7f608dcf4
Jdentity <identityjang@gmail.com>
2023-01-27 21:29:17 +0900

    🧐 Add GA basic ga initialize 추가 라우팅 추적 추가

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import ReactGA from 'react-ga';
 import ToastProvider from './components/toast/ToastProvider';
 import App from './App';
 import theme from './styles/theme';
 import Globalstyle from './styles/GlobalStyle';
 import { store, persistor } from './app/configStore';
 import './styles/font.css';
+
+if (process.env.REACT_APP_GOOGLE_ANALYSTICS_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYSTICS_ID);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
