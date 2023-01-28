0dc5bd12acd755e80823de2e258d30b0505c2bc1
Jdentity <identityjang@gmail.com>
2023-01-29 04:36:09 +0900

    👔 Chage data type of result to object 결과 오브젝트형태로 변경 컴포넌트 분리

---
+++ src/components/gameResult/KeywordResult.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function KeywordResult({ nickname, userImg, keyword }) {
+  return (
+    <StKeywordResult>
+      <KeywordNickname>{nickname}</KeywordNickname>
+      <KeywordWrapper>
+        <ProfileImg src={userImg} alt="profile" />
+        <Keyword>{keyword}</Keyword>
+      </KeywordWrapper>
+    </StKeywordResult>
+  );
+}
+
+const StKeywordResult = styled.div``;
+
+const KeywordWrapper = styled.div`
+  margin-top: 10px;
+  align-items: center;
+  display: flex;
+`;
+
+const KeywordNickname = styled.div`
+  width: max-content;
+  margin-left: 65px;
+  margin-top: 18px;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  color: ${({ theme }) => theme.colors.DARK_LAVA};
+`;
+
+const Keyword = styled.pre`
+  color: ${({ theme }) => theme.colors.DARK_LAVA};
+  display: block;
+  width: max-content;
+  padding: 15px;
+  margin-left: 15px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.ANTIQUE_WHITE};
+  line-height: 20px;
+`;
+
+const ProfileImg = styled.img`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+
+export default KeywordResult;
