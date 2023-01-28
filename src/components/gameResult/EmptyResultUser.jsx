4b518dcc24fe79ff1000fb736d7fe0ca2be89df1
Jdentity <identityjang@gmail.com>
2023-01-28 15:30:44 +0900

    💄 Update emptyresultuser style 빈느낌이 나도록 스타일 수정

--- src/components/gameResult/EmptyResultUser.jsx
+++ src/components/gameResult/EmptyResultUser.jsx
@@ -2,29 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 function EmptyResultUser() {
-  return (
-    <User>
-      <ProfileImg />
-    </User>
-  );
+  return <User />;
 }
 
 const User = styled.div`
   padding: 8px;
-  align-items: center;
   display: flex;
+  align-items: center;
   flex-direction: row;
   width: 100%;
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.FLORAL_WHITE};
-`;
-
-const ProfileImg = styled.div`
-  border-radius: 50%;
-  background-color: #1290cb;
-  padding: 25px;
-  width: max-content;
-  height: max-content;
+  filter: brightness(0.9);
 `;
 
 export default EmptyResultUser;
