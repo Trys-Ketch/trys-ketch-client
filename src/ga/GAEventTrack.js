700ebe7bf7f36c6b060148dd2834cb2378797980
Jdentity <identityjang@gmail.com>
2023-01-29 17:08:00 +0900

    ⬆️ Migrate react-ga to version 4

--- src/ga/GAEventTrack.js
+++ src/ga/GAEventTrack.js
@@ -1,4 +1,4 @@
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const GAEventTrack = (category, action, label) => {
   ReactGA.event({
--- src/ga/GARouteTracker.jsx
+++ src/ga/GARouteTracker.jsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
+/**
+ * uri 변경 추적 컴포넌트
+ * uri가 변경될 때마다 pageview 이벤트 전송
+ */
 const GARouteTracker = () => {
   const location = useLocation();
   const [initialized, setInitialized] = useState(false);
 
+  // localhost는 기록하지 않음
   useEffect(() => {
-    // 구글 애널리틱스 운영서버만 적용
-    if (process.env.REACT_APP_GOOGLE_ANALYSTICS_ID) {
-      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYSTICS_ID);
+    if (!window.location.href.includes('localhost')) {
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRAKING_ID);
+      setInitialized(true);
     }
-    setInitialized(true);
   }, []);
 
+  // location 변경 감지시 pageview 이벤트 전송
   useEffect(() => {
     if (initialized) {
-      ReactGA.pageview(location.pathname + location.search);
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send('pageview');
     }
   }, [initialized, location]);
 };
