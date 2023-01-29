51bcf763fd585c98a146971c371332ce7ee6b52e
Jdentity <identityjang@gmail.com>
2023-01-29 20:23:53 +0900

    🚑 Hotfix ga id ga id 환경변수 이름을 잘못넣어서 수정

--- src/ga/GARouteTracker.jsx
+++ src/ga/GARouteTracker.jsx
@@ -13,7 +13,7 @@ const GARouteTracker = () => {
   // localhost는 기록하지 않음
   useEffect(() => {
     if (!window.location.href.includes('localhost')) {
-      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRAKING_ID);
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYSTICS_ID);
       setInitialized(true);
     }
   }, []);
