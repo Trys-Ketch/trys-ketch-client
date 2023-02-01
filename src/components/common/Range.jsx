52e79419ca98e7fe9c6cb3770c596d28317da78a
Jdentity <identityjang@gmail.com>
2023-02-01 22:56:42 +0900

    💄 Feat SettingModal 설정 모달 오픈! 사운드 전역 관리

--- src/components/common/Range.jsx
+++ src/components/common/Range.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Range({ width, min, max, value, onChange }) {
+function Range({ width, min, max, value, onChange, ...rest }) {
+  const htmlProps = rest;
   return (
-    <InputRange type="range" width={width} min={min} max={max} value={value} onChange={onChange} />
+    <InputRange
+      type="range"
+      width={width}
+      min={min}
+      max={max}
+      value={value}
+      onChange={onChange}
+      {...htmlProps}
+    />
   );
 }
 
@@ -19,93 +28,5 @@ const InputRange = styled.input`
     outline: none;
   }
 `;
-
-// const InputRange = styled.input`
-//   overflow: hidden;
-//   display: block;
-//   appearance: none;
-//   max-width: 700px;
-//   width: 100%;
-//   margin: 0;
-//   height: 10px;
-//   cursor: pointer;
-//   background: #4e473f;
-
-//   &:focus {
-//     outline: none;
-//   }
-
-//   &::-webkit-slider-runnable-track {
-//     width: 100%;
-//     height: 10px;
-//     background: '#4e473f';
-//   }
-
-//   &::-webkit-slider-thumb {
-//     position: relative;
-//     appearance: none;
-//     height: 36px;
-//     width: 36px;
-//     background: '#4e473f';
-//     border-radius: 100%;
-//     border: 0;
-//     top: 50%;
-//     transform: translateY(-50%);
-//     transition: background-color 150ms;
-//   }
-
-//   &::-moz-range-track,
-//   &::-moz-range-progress {
-//     width: 100%;
-//     height: 36px;
-//     background: #edf5f9;
-//   }
-
-//   &::-moz-range-progress {
-//     background: #0199ff;
-//   }
-
-//   &::-moz-range-thumb {
-//     appearance: none;
-//     margin: 0;
-//     height: 36;
-//     width: 36;
-//     background: '#4e473f';
-//     border-radius: 100%;
-//     border: 0;
-//     transition: background-color 150ms;
-//   }
-
-//   &::-ms-track {
-//     width: 100%;
-//     height: 36px;
-//     border: 0;
-//     /* color needed to hide track marks */
-//     color: transparent;
-//     background: transparent;
-//   }
-
-//   &::-ms-fill-lower {
-//     background: #0199ff;
-//   }
-
-//   &::-ms-fill-upper {
-//     background: #edf5f9;
-//   }
-
-//   &::-ms-thumb {
-//     appearance: none;
-//     height: 36;
-//     width: 36;
-//     background: '#4e473f';
-//     border-radius: 100%;
-//     border: 0;
-//     transition: background-color 150ms;
-//     /* IE Edge thinks it can support -webkit prefixes */
-//     top: 0;
-//     margin: 0;
-//     box-shadow: none;
-//   }
-// `;
 
 export default Range;
--- src/components/modal/SettingModal.jsx
+++ src/components/modal/SettingModal.jsx
@@ -1,39 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
 import Modal from '../common/Modal';
-import Switch from '../common/Switch';
 import Range from '../common/Range';
 import useModal from '../../hooks/useModal';
 import speaker from '../../assets/icons/speaker-icon.svg';
-// import speakerMute from '../../assets/icons/speaker-mute-icon.svg';
-import mic from '../../assets/icons/mic-icon.svg';
+import speakerMute from '../../assets/icons/speaker-mute-icon.svg';
 import GAEventTrack from '../../ga/GAEventTrack';
 import GAEventTypes from '../../ga/GAEventTypes';
-// import micMute from '../../assets/icons/mic-mute-icon.svg';
+import { setVolume } from '../../app/slices/soundSlice';
 
 function SettingModal() {
   const { closeModal } = useModal();
-  // const { musicVolume, setMusicVolume } = useState();
-  // const { micVolume, setMicVolume } = useState();
+  const dispatch = useDispatch();
+  const sound = useSelector((state) => state.sound);
+  const [range, setRange] = useState(sound.volume);
+
+  const handleRange = (e) => {
+    setRange(e.target.value);
+  };
 
   const handleEnter = () => {
     GAEventTrack(GAEventTypes.Category.setting, GAEventTypes.Action.setting.Settings);
     closeModal();
   };
 
+  useEffect(() => {
+    dispatch(setVolume(range));
+  }, [dispatch, range]);
+
   return (
     <Modal title="설정" btnText="완료" onConfirm={handleEnter}>
       <Row>
-        <Typography>배경음악</Typography>
-        <Switch />
+        <Typography>전체음량</Typography>
       </Row>
       <Row>
-        <img src={speaker} alt="speaker" />
-        <Range width="85%" />
-      </Row>
-      <Row>
-        <img src={mic} alt="mic" />
-        <Range width="85%" />
+        <SoundButton>
+          {range === '0' ? (
+            <img src={speakerMute} alt="speaker-mute" />
+          ) : (
+            <img src={speaker} alt="speaker" />
+          )}
+        </SoundButton>
+        <Range width="85%" min="0" max="1" step="0.01" value={range} onChange={handleRange} />
       </Row>
     </Modal>
   );
@@ -46,9 +55,12 @@ const Row = styled.div`
   width: 100%;
 `;
 
+const SoundButton = styled.button``;
+
 const Typography = styled.h3`
   font-family: 'TTTogether';
   font-size: ${({ theme }) => theme.fontSizes.xxl};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
   color: ${({ theme }) => theme.colors.DIM_GRAY};
 `;
 
