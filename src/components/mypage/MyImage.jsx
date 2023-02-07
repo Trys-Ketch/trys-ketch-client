e061bbd14551d95e8c1e00b5ec29f2a552111e69
Jdentity <identityjang@gmail.com>
2023-02-07 15:01:40 +0900

    💄 Fix one & two line my images 좋아요한 이미지가 한줄 두줄일때 height가 바뀌는 부분 수정

--- src/components/mypage/MyImage.jsx
+++ src/components/mypage/MyImage.jsx
@@ -11,6 +11,8 @@ function MyImage({ isFetching, hasNextPage, fetchNextImage, images }) {
     }
   });
 
+  console.log(images);
+
   if (images.length === 0) {
     return (
       <>
@@ -38,6 +40,7 @@ const Target = styled.div`
 const ImageWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(auto-fill, minmax(156px, 156px));
   gap: 20px;
   width: 100%;
   height: 100%;
@@ -52,6 +55,8 @@ const Canvas = styled.div`
   img {
     width: 100%;
     height: 100%;
+    max-width: 100%;
+    max-height: 100%;
     object-fit: contain;
   }
 `;
