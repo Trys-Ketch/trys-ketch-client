1b6145b5b14786eab950aa1d36e4dd907fd8a3d9
Jdentity <identityjang@gmail.com>
2023-02-14 10:55:50 +0900

    ♻️ Refactor gameroom utils GA관련, sound 관련은 컴포넌트에서 작동하는게 더욱 독립적임 유틸 함수는 순수하게 유틸 관련 코드만 남아있도록 수정

--- src/utils/gameRoomStompUtils.js
+++ src/utils/gameRoomStompUtils.js
@@ -1,18 +1,13 @@
-import GAEventTrack from '../ga/GAEventTrack';
-import GAEventTypes from '../ga/GAEventTypes';
-
-const toggleReady = (socket, id, readySoundRef) => {
+function toggleReady(socket, id) {
   socket.send(JSON.stringify({ type: 'ingame/toggle_ready', room: id }));
-  readySoundRef.current.play();
-};
+}
 
-const start = (ingameStompClient, id, token) => {
+function start(ingameStompClient, id, token) {
   ingameStompClient.publish({
     destination: '/app/game/start',
     body: JSON.stringify({ roomId: id, token }),
   });
-  GAEventTrack(GAEventTypes.Category.game, GAEventTypes.Action.game.startGame);
-};
+}
 
 function sendDifficulty(difficulty, ingameStompClient, id, token) {
   ingameStompClient.publish({
